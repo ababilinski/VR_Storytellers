1d27c8b481d708e2fa6a2d07c4dea86bb3d97560
Adrian Babilinski <ababilinski@gmail.com>
2016-10-08 13:31:47 -0400

    Added json parsing

---
+++ Assets/Application/Scripts/Core/Story.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[System.Serializable]
+public class Story {
+    public string discription { get; private set; }
+    public float time { get; private set; }
+    public string mood { get; private set; }
+    public string location { get; private set; }
+    public List<GameObject> sceneObjects { get; private set; }
+
+    public Story(string discripion, float time, string mood, string location, List<GameObject> sceneObjects) {
+        this.discription = discription;
+        this.time = time;
+        this.mood = mood;
+        this.location = location;
+        this.sceneObjects = sceneObjects;
+    }
+}
