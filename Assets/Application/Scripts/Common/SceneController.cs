b740d58f97f76f0da3e8c84bbd3774eb7edbb9f0
Adrian Babilinski <ababilinski@gmail.com>
2016-10-08 13:54:02 -0400

    Added scene controller

---
+++ Assets/Application/Scripts/Common/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public static SceneController Loader;
+
+
+    void Awake() {
+        if (Loader == null) {
+            Loader = this;
+        }
+        else if (Loader != null && Loader != this) {
+            Destroy(this);
+        }
+    }
+
+   public Coroutine LoadScene(string sceneName) {
+        return StartCoroutine(LoadSceneCoroutine(sceneName));
+       
+    }
+
+
+    IEnumerator LoadSceneCoroutine(string sceneName) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!operation.isDone) {
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
